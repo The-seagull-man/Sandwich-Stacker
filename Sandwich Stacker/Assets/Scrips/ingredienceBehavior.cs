6603c4fa9ca30f8f729ec3bd92bb9fd91c1b5988
The-seagull-man <mail@marenvalentin.dk>
2025-10-31 10:05:10 +0100

    behavior

--- Sandwich Stacker/Assets/Scrips/ingredienceBehavior.cs
+++ Sandwich Stacker/Assets/Scrips/ingredienceBehavior.cs
@@ -5,8 +5,12 @@ public class ingredienceBehavior : MonoBehaviour
 {
     [SerializeField] private float speed;
     bool fly = true;
+    public listOfIngredience playerListOfIngredience;
 
-    bool collision = false;
+    bool collisionBool = false;
+    bool touchedTerminator = false;
+    bool touchedIngredient = false;
+    bool touchedStacker = false;
    
     void Start()
     {
@@ -14,7 +18,7 @@ public class ingredienceBehavior : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space) && transform.position.x >= -9 && transform.position.x <= 9)
         {
@@ -26,18 +30,38 @@ public class ingredienceBehavior : MonoBehaviour
             transform.position += Vector3.right * speed;
         }
 
-        if (collision == false && fly == false)
+        if (collisionBool == false && fly == false)
         {
             transform.position += Vector3.down * speed;
         }
-    }
 
-    void DropSandwich()
-    {
-        fly = false;
-        while(collision == false)
+        if (touchedIngredient && touchedStacker == true)
         {
-            transform.position += Vector3.down * speed;
+            collisionBool = true;
+            playerListOfIngredience.theIngredienceList.Add(gameObject);
+        }
+    }
+  
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.layer == 3)
+        {
+            touchedIngredient = true;
+        }
+       
+        else if (collision.gameObject.layer == 7 && (touchedStacker && touchedIngredient) == true)
+        {
+            Object.Destroy(gameObject);
+            Debug.Log("diller");
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            touchedStacker = true;
         }
     }
 }
