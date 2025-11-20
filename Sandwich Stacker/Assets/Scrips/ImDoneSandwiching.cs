e4f9d41aca0b0ef14b1a08116a4145d3b7fe0bc8
The-seagull-man <mail@marenvalentin.dk>
2025-11-20 08:58:34 +0100

    oliven

---
+++ Sandwich Stacker/Assets/Scrips/ImDoneSandwiching.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ImDoneSandwiching : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject spawner;
+    public GameObject sandwichParent;
+    public GameObject plate;
+    [SerializeField] private float speed = 1.0f;
+    private float step;
+    bool sandwichIsDone = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed*Time.deltaTime;
+
+        if (sandwichIsDone == true)
+        {
+            sandwichParent.transform.position = Vector3.MoveTowards(sandwichParent.transform.position, plate.transform.position, step);
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        spawner.GetComponent<Spawner>().StopSpawning();
+        PresentSandwich();
+        sandwichIsDone=true;
+    }
+
+    void PresentSandwich()
+    {
+    }
+}
--- Sandwich Stacker/Assets/Scrips/Spawner.cs
+++ Sandwich Stacker/Assets/Scrips/Spawner.cs
@@ -1,10 +1,13 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public InList ingredientsToChoose;
+    public Transform parentSandwich;
     private Vector3 pos = new Vector3(-12,3,0);
     private float countUp = 0;
+    private bool isStopped = false;
     private int orderToIngredient = 0;
     private GameObject randomObject;
     private Transform top;
@@ -17,7 +20,7 @@ public class Spawner : MonoBehaviour
     void FixedUpdate()
     {
         countUp += Time.deltaTime;
-        if (countUp >= 4)
+        if (countUp >= 4 && isStopped == false)
         {
             SpawnIngredient();
             countUp = 0;
@@ -26,12 +29,16 @@ public class Spawner : MonoBehaviour
 
     public void SpawnIngredient()
     {
-        randomObject = Instantiate(ingredientsToChoose.gameObjects[Random.Range(0, 7)], pos, Quaternion.identity); 
+        randomObject = Instantiate(ingredientsToChoose.gameObjects[Random.Range(0, 7)], pos, Quaternion.identity,parentSandwich); 
         randomObject.GetComponent<SpriteRenderer>().sortingOrder = orderToIngredient;
         top = randomObject.transform.Find("Top");
         top.GetComponent<SpriteRenderer>().sortingOrder = orderToIngredient; 
         orderToIngredient++;
     }
 
+    public void StopSpawning()
+    {
+        isStopped = true;
+    }
 
 }
