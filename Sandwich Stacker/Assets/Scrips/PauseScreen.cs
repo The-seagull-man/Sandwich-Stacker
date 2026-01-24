57ccb99a729396e498eb48c66c81b4fd4be2fbbb
The-seagull-man <mail@marenvalentin.dk>
2026-01-24 23:11:10 +0100

    menu

---
+++ Sandwich Stacker/Assets/Scrips/PauseScreen.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    public Canvas pauseScreenCanvas;
+    void Start()
+    {
+        pauseScreenCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreenCanvas.gameObject.activeSelf == true)
+            {
+                ReturnFromPause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void ReturnFromPause()
+    {
+        pauseScreenCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+
+    }
+
+    public void Pause()
+    {
+        pauseScreenCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
