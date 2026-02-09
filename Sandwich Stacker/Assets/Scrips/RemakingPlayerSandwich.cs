11795ea8d46a91d5653f787550573e9aa6dddca3
Maren Valentin <150529308+The-seagull-man@users.noreply.github.com>
2026-02-09 12:07:55 +0100

    noget idk

--- Sandwich Stacker/Assets/Scrips/RemakingPlayerSandwich.cs
+++ Sandwich Stacker/Assets/Scrips/RemakingPlayerSandwich.cs
@@ -9,23 +9,28 @@ public class RemakingPlayerSandwich : MonoBehaviour
     void Start()
     {
         comp = GetComponentsInChildren<SpriteRenderer>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
         for (int i = 0; i < comp.Length; i++)
         {
+            Debug.Log("Yes");
 
             if (i < PlayerSandwich.gameObjects.Count)
             {
+                Debug.Log("okay");
+
                 for (int j = 0; j < sandwichIngredients.gameObjects.Count; j++)
                 {
+                    Debug.Log("alr");
+
+                    Debug.Log("player"+PlayerSandwich.gameObjects[i].GetComponent<SpriteRenderer>().tag);
+                    Debug.Log("ingredients:" + sandwichIngredients.gameObjects[j].GetComponent<SpriteRenderer>().tag);
+
+
                     if (PlayerSandwich.gameObjects[i].GetComponent<SpriteRenderer>().tag == sandwichIngredients.gameObjects[j].GetComponent<SpriteRenderer>().tag)
                     {
-                        comp[i].GetComponent<SpriteRenderer>().sprite  = sandwichIngredients.gameObjects[j].GetComponent<SpriteRenderer>().sprite;
+                        comp[i].GetComponent<SpriteRenderer>().sprite = sandwichIngredients.gameObjects[j].GetComponent<SpriteRenderer>().sprite;
+                        Debug.Log("burde virke?");
+
                     }
-                    //Debug.Log(i);
                 }
             }
             else
@@ -33,5 +38,11 @@ public class RemakingPlayerSandwich : MonoBehaviour
                 comp[i].GetComponent<SpriteRenderer>().sprite = null;
             }
         }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
     }
 }
