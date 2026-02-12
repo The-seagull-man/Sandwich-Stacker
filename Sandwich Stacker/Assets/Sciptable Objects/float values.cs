6acc25361b207855a52f0a7eb8a9d733fefe7b8b
Brotospingvin <153622518+Brotospingvin@users.noreply.github.com>
2026-02-12 15:50:45 +0100

    score

---
+++ Sandwich Stacker/Assets/Sciptable Objects/float values.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "floatvalues", menuName = "Scriptable Objects/floatvalues")]
+public class floatvalues : ScriptableObject
+{
+    public float IngredientCorrectness;
+    public float HowTheFuckGoodStackedItIs;
+}
