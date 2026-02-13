de3e7552beb8756cd94bc4dac74b5e4c9dc69070
Maren Valentin <150529308+The-seagull-man@users.noreply.github.com>
2026-02-13 19:30:44 +0100

    fikset oliven og pos

--- Sandwich Stacker/Assets/Scrips/RemakingPlayerSandwich.cs
+++ Sandwich Stacker/Assets/Scrips/RemakingPlayerSandwich.cs
@@ -5,7 +5,9 @@ public class RemakingPlayerSandwich : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public InList PlayerSandwich;
     public Component[] comp;
-    public Component olive;
+    public GameObject olive;
+    public ListFloat trackedPos;
+    private GameObject spawnedOlive;
     void Start()
     {
         comp = GetComponentsInChildren<SpriteRenderer>();
@@ -14,12 +16,19 @@ public class RemakingPlayerSandwich : MonoBehaviour
 
             if (i < PlayerSandwich.gameObjects.Count)
             {
-                        comp[i].GetComponent<SpriteRenderer>().sprite = PlayerSandwich.gameObjects[i].GetComponent<SpriteRenderer>().sprite;
+                comp[i].GetComponent<SpriteRenderer>().sprite = PlayerSandwich.gameObjects[i].GetComponent<SpriteRenderer>().sprite;
+                comp[i].transform.position += new Vector3(trackedPos.positions[i], 0, 0);
+
             }
             else
             {
                
                 comp[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            if (i+1 == PlayerSandwich.gameObjects.Count)
+            {
+                spawnedOlive = Instantiate(olive, comp[i].transform);
+                spawnedOlive.transform.position += new Vector3(0, 0.5f, 0);
             }
         }
         
